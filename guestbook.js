814b9f110876dc73124739bca733fcb07bc0771e
Jacobus-afk <62353676+Jacobus-afk@users.noreply.github.com>
2022-03-19 15:09:38 +0200

    updated apiUrl() with cloud fn api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9622d45b.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
